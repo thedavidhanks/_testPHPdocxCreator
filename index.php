e47281747bcd4e2492b2d4988ced1ac9dec274fa
dhanks <dhanks@DHANKS.explore.local>
2018-04-17 16:22:49 -0500

    More rows in pipe detail calculations

--- index.php
+++ index.php
@@ -169,17 +169,109 @@ $sectionMainContent->addPageBreak();
 
 //Pipe Detail Calculations
 foreach($pipesArray as $pipeData){  //see 3-pipeDataSummary for $pipesArray
+        $gradeName = $pipeData->strengthType === "grade" ? $pipeData->grade : "";
+        $pipeDiameterStr = $pipeData->diameter;
+        $pipeWallStr = $pipeData->wall;
 	if($pipeData->pipeNo === 0){
-		$sectionMainContent->addTitle('Test Pipe', 1);
-	}else{$sectionMainContent->addTitle('Pipe No. '.$pipeData->pipeNo, 1);}
+		$sectionMainContent->addTitle('Test Pipe: ', 1);
+        }else{$sectionMainContent->addTitle("Pipe No. {$pipeData->pipeNo}: {$gradeName}, {$pipeDiameterStr}\" OD, {$pipeWallStr}\" Wall", 1);}
 	$tablePipeCalculations = $sectionMainContent->addTable('tsPlain');
-	$tablePipeCalculations->addRow($rsHeight);  //I guess I can't configure a row.
-	$tablePipeCalculations->addCell(Converter::inchtotwip(2.62),$csVbottomGrey)->addText("Definitions", 'fsNormal', 'psCenterTable3');
+	$tablePipeCalculations->addRow($rsHeight); 
+	$tablePipeCalculations->addCell(Converter::inchtotwip(2.62),$csVbottomGrey)->addText("Definition", 'fsNormal', 'psCenterTable3');
 	$tablePipeCalculations->addCell(Converter::inchtotwip(.62),$csVbottomGrey)->addText("Term", 'fsNormal', 'psCenterTable3');
 	$tablePipeCalculations->addCell(Converter::inchtotwip(2.58),$csVbottomGrey)->addText("Equation", 'fsNormal', 'psCenterTable3');
 	$tablePipeCalculations->addCell(Converter::inchtotwip(.71),$csVbottomGrey)->addText("Result", 'fsNormal', 'psCenterTable3');
 	$tablePipeCalculations->addCell(Converter::inchtotwip(.46),$csVbottomGrey)->addText("Units", 'fsNormal', 'psCenterTable3');
-	
+	$tablePipeCalculations->addRow($rsHeight);
+        $tablePipeCalculations->addCell(null, $vAlignCell)->addText("Evaluation Yield Strength", 'fsNormal', 'psLeftTable3');
+        addTermCell($tablePipeCalculations,"S","y");
+        $tablePipeCalculations->addCell(null,$csGrey);
+        $tablePipeCalculations->addCell(null, $vAlignCell)->addText($pipeData->evalStrength, 'fsNormal', 'psCenterTable3');
+        $tablePipeCalculations->addCell(null, $vAlignCell)->addText('psi', 'fsNormal', 'psCenterTable3');
+        $tablePipeCalculations->addRow($rsHeight);
+        $tablePipeCalculations->addCell(null, $vAlignCell)->addText("Outside Diameter of Pipe", 'fsNormal', 'psLeftTable3');
+        $tablePipeCalculations->addCell(null, $vAlignCell)->addText("OD", 'fsNormal', 'psCenterTable3');
+        $tablePipeCalculations->addCell(null,$csGrey);
+        $tablePipeCalculations->addCell(null, $vAlignCell)->addText($pipeDiameterStr, 'fsNormal', 'psCenterTable3');
+        $tablePipeCalculations->addCell(null, $vAlignCell)->addText('in', 'fsNormal', 'psCenterTable3');
+        $tablePipeCalculations->addRow($rsHeight);
+        if($pipeData->selectedMethod =="Cameron"){
+            $tablePipeCalculations->addCell(null, $vAlignCell)->addText("Pipe weight", 'fsNormal', 'psLeftTable3');
+            $tablePipeCalculations->addCell(null, $vAlignCell)->addText("ppf", 'fsNormal', 'psCenterTable3');
+            $tablePipeCalculations->addCell(null,$csGrey);
+            $tablePipeCalculations->addCell(null, $vAlignCell)->addText($pipeData->ppf, 'fsNormal', 'psCenterTable3');
+            $tablePipeCalculations->addCell(null, $vAlignCell)->addText('ppf', 'fsNormal', 'psCenterTable3');
+            $tablePipeCalculations->addRow($rsHeight);
+            $tablePipeCalculations->addCell(null, $vAlignCell)->addText("Shearing Constant", 'fsNormal', 'psLeftTable3');
+            addTermCell($tablePipeCalculations,"C","3");
+            $tablePipeCalculations->addCell(null,$csGrey);
+            $tablePipeCalculations->addCell(null, $vAlignCell)->addText($pipeData->C3, 'fsNormal', 'psCenterTable3');
+            $tablePipeCalculations->addCell(null,$csGrey);
+            $tablePipeCalculations->addRow($rsHeight);
+            $tablePipeCalculations->addCell(null, $vAlignCell)->addText("Calculated Force to Shear at Surface", 'fsNormal', 'psLeftTable3');
+            addTermCell($tablePipeCalculations,"F","CAM");
+            $tablePipeCalculations->addCell(null, $vAlignCell)->addText("C3 x Sy x ppf");
+            $tablePipeCalculations->addCell(null, $vAlignCell)->addText($pipeData->CamForce, 'fsNormal', 'psCenterTable3');
+            $tablePipeCalculations->addCell(null, $vAlignCell)->addText('kips', 'fsNormal', 'psCenterTable3');
+            $tablePipeCalculations->addRow($rsHeight);
+            $tablePipeCalculations->addCell(null, $vAlignCell)->addText("Pressure Required at Sy", 'fsNormal', 'psLeftTable3');
+            addTermCell($tablePipeCalculations,"P","CAM");
+            $tablePipeCalculations->addCell(null, $vAlignCell)->addText("Fcam / Ac x 1000 lb/kip");
+            $tablePipeCalculations->addCell(null, $vAlignCell)->addText('xxx', 'fsNormal', 'psCenterTable3');
+            $tablePipeCalculations->addCell(null, $vAlignCell)->addText('psi', 'fsNormal', 'psCenterTable3');
+            
+        }else if($pipeData->selectedMethod =="West"){
+            $tablePipeCalculations->addCell(null, $vAlignCell)->addText("Nominal Thickness of Pipe", 'fsNormal', 'psLeftTable3');
+            $tablePipeCalculations->addCell(null, $vAlignCell)->addText("Wall", 'fsNormal', 'psCenterTable3');
+            $tablePipeCalculations->addCell(null,$csGrey);
+            $tablePipeCalculations->addCell(null, $vAlignCell)->addText($pipeWallStr, 'fsNormal', 'psCenterTable3');
+            $tablePipeCalculations->addCell(null, $vAlignCell)->addText('in', 'fsNormal', 'psCenterTable3');
+            $tablePipeCalculations->addRow($rsHeight);
+            $tablePipeCalculations->addCell(null, $vAlignCell)->addText("Pipe Cross-Sectional Area", 'fsNormal', 'psLeftTable3');
+            addTermCell($tablePipeCalculations,"A","g");
+            $tablePipeCalculations->addCell(null, $vAlignCell)->addText('OD2 - (OD - 2 x Wall)^2 x pi / 4', 'fsNormal', 'psCenterTable3');
+            $tablePipeCalculations->addCell(null, $vAlignCell)->addText($pipeData->area, 'fsNormal', 'psCenterTable3');
+            addSqInchCell($tablePipeCalculations);
+            $tablePipeCalculations->addRow($rsHeight);
+            $tablePipeCalculations->addCell(null, $vAlignCell)->addText("Calculated Force to Shear at Surface", 'fsNormal', 'psLeftTable3');
+            addTermCell($tablePipeCalculations,"F","YS");
+            $tablePipeCalculations->addCell(null, $vAlignCell)->addText($pipeData->WestDef);
+            $tablePipeCalculations->addCell(null, $vAlignCell)->addText($pipeData->WestForce, 'fsNormal', 'psCenterTable3');
+            $tablePipeCalculations->addCell(null, $vAlignCell)->addText('kips', 'fsNormal', 'psCenterTable3');
+            $tablePipeCalculations->addRow($rsHeight);
+            $tablePipeCalculations->addCell(null, $vAlignCell)->addText("Pressure Required at Sy", 'fsNormal', 'psLeftTable3');
+            addTermCell($tablePipeCalculations,"P","WEST");
+            $tablePipeCalculations->addCell(null, $vAlignCell)->addText("Fwest / Ac x 1000 lb/kip");
+            $tablePipeCalculations->addCell(null, $vAlignCell)->addText('xxx', 'fsNormal', 'psCenterTable3');
+            $tablePipeCalculations->addCell(null, $vAlignCell)->addText('psi', 'fsNormal', 'psCenterTable3');
+        }
+        $tablePipeCalculations->addRow($rsHeight);
+        $tablePipeCalculations->addCell(null, $vAlignCell)->addText("Total Calculated Shear Pressure adjusted for operator effects", 'fsNormal', 'psLeftTable3');
+        addTermCell($tablePipeCalculations,"P","T");
+        $tablePipeCalculations->addCell(null, $vAlignCell)->addText("P + Padj");
+        $tablePipeCalculations->addCell(null, $vAlignCell)->addText($pipeData->OperatingPressure, 'fsNormal', 'psCenterTable3');
+        $tablePipeCalculations->addCell(null, $vAlignCell)->addText('psi', 'fsNormal', 'psCenterTable3');
+        $tablePipeCalculations->addRow($rsHeight);
+        $tablePipeCalculations->addCell(null, $vAlignCell)->addText("Operating Pressure to Shear and Seal", 'fsNormal', 'psLeftTable3');
+        addTermCell($tablePipeCalculations,"P","OP");
+        $tablePipeCalculations->addCell(null, $vAlignCell)->addText("MAX of Pys and Pseal");
+        $tablePipeCalculations->addCell(null, $vAlignCell)->addText($pipeData->OperatingPressure, 'fsNormal', 'psCenterTable3');
+        $tablePipeCalculations->addCell(null, $vAlignCell)->addText('psi', 'fsNormal', 'psCenterTable3');
+        $tablePipeCalculations->addRow($rsHeight);
+        $tablePipeCalculations->addCell(null, $vAlignCell)->addText("Is the calculated pressure less than or equal to 90% of the max operating pressure of the BOP?", 'fsNormal', 'psLeftTable3');
+        $tablePipeCalculations->addCell(null,$csGrey);
+        $tablePipeCalculations->addCell(null,$csGrey);
+        $tablePipeCalculations->addCell(null,$csSpan2Good);
+        $tablePipeCalculations->addRow($rsHeight);
+        $tablePipeCalculations->addCell(null, $vAlignCell)->addText("Is the calculated pressure less than or equal to the supply pressure?", 'fsNormal', 'psLeftTable3');
+        $tablePipeCalculations->addCell(null,$csGrey);
+        $tablePipeCalculations->addCell(null,$csGrey);
+        $tablePipeCalculations->addCell(null,$csSpan2Good);
+        $tablePipeCalculations->addRow($rsHeight);
+        $tablePipeCalculations->addCell(null, $vAlignCell)->addText("Is the calculated pressure less than or equal to the calculated pressure of the test pipe?", 'fsNormal', 'psLeftTable3');
+        $tablePipeCalculations->addCell(null,$csGrey);
+        $tablePipeCalculations->addCell(null,$csGrey);
+        $tablePipeCalculations->addCell(null,$csSpan2Good);
 	$sectionMainContent->addPageBreak();
 }
 
